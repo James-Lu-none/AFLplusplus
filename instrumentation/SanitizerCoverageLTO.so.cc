b8dc3f9a23864fae0703c4a6c7909527e2013af7
James-Lu-none <jamesabcde277@gmail.com>
2026-02-22 21:17:02 +0800

    move report target batch callee instrumentation from toinstrumentFunction to avoid block by shouldInstrumentBlock

--- instrumentation/SanitizerCoverageLTO.so.cc
+++ instrumentation/SanitizerCoverageLTO.so.cc
@@ -464,7 +464,7 @@ bool ModuleSanitizerCoverageLTO::instrumentModule(
 
     if (!TargetBB) continue;
 
-    fprintf(stderr, "[LTO-BFS] Found Target! Starting Global Distance Calculation...\n");
+    fprintf(stderr, "[LTO-BFS] Found Target for %s:%d! Starting Global Distance Calculation...\n", T.filename.c_str(), T.line);
     std::deque<BasicBlock *>         WorkList;
     std::map<BasicBlock *, uint32_t> Distances;
     Distances[TargetBB] = 0;
@@ -503,7 +503,7 @@ bool ModuleSanitizerCoverageLTO::instrumentModule(
         }
       }
     }
-    fprintf(stderr, "[LTO-BFS] Distance Calculation Complete. Total BBs mapped: %zu\n", BBToTargetsMap.size());
+    fprintf(stderr, "[LTO-BFS] Distance Calculation Complete for target %s:%d. Total BBs mapped: %zu\n", T.filename.c_str(), T.line, BBToTargetsMap.size());
   }
   // 不在這邊做 instrumentation，等到真正跑到 InjectCoverage 的時候再根據 GlobalDistances 決定要不要插入 ReportHitFunc，這樣就不會對不可達的 basic block 造成額外的 overhead
   // for (auto &F : M) {
@@ -2221,6 +2221,44 @@ void ModuleSanitizerCoverageLTO::instrumentFunction(
 
     }
 
+    // 在這裡插樁可以避免被shouldInstrumentBlock擋掉
+    // 檢查此 BB 是否在距離地圖中
+    auto it = BBToTargetsMap.find(&BB);
+    if (it != BBToTargetsMap.end() && !it->second.empty()) {
+      auto    &TargetList = it->second;
+      uint32_t num_targets = TargetList.size();
+
+      // 建立一個常數陣列包含 [ID0, Dist0, ID1, Dist1, ...]
+      std::vector<Constant *> ArrayElems;
+      for (auto &p : TargetList) {
+        std::string              bbName;
+        llvm::raw_string_ostream rso(bbName);
+        BB.printAsOperand(rso, false);
+        if (Instruction *I = BB.getFirstNonPHI()) {
+          if (DILocation *Loc = I->getDebugLoc()) {
+            fprintf(stderr,
+                    "DEBUG: Instrumenting BB %s:%d in BB %s with target ID %u "
+                    "and distance %u\n",
+                    Loc->getFilename().str().c_str(), Loc->getLine(),
+                    bbName.c_str(), p.first, p.second);
+          }
+        }
+        ArrayElems.push_back(ConstantInt::get(Int32Ty, p.first));   // target ID
+        ArrayElems.push_back(ConstantInt::get(Int32Ty, p.second));  // Distance
+      }
+
+      ArrayType      *ArrTy = ArrayType::get(Int32Ty, num_targets * 2);
+      GlobalVariable *GDistArray = new GlobalVariable(
+          *CurModule, ArrTy, true, GlobalValue::InternalLinkage,
+          ConstantArray::get(ArrTy, ArrayElems), ".afl_target_dists");
+
+      Value         *ArrayPtr = IRB.CreatePointerCast(GDistArray, PtrTy);
+      FunctionCallee ReportBatchFunc = CurModule->getOrInsertFunction(
+          "__afl_report_target_batch", Type::getVoidTy(*C), Int32Ty, PtrTy);
+      IRB.CreateCall(ReportBatchFunc,
+                     {ConstantInt::get(Int32Ty, num_targets), ArrayPtr});
+    }
+
     if (!instrument_ctx)
       if (shouldInstrumentBlock(F, &BB, DT, PDT, Options))
         BlocksToInstrument.push_back(&BB);
@@ -2382,31 +2420,6 @@ void ModuleSanitizerCoverageLTO::InjectCoverageAtBlock(Function   &F,
   }
 
   IRBuilder<> IRB(&*IP);
-
-  // 檢查此 BB 是否在距離地圖中
-  auto it = BBToTargetsMap.find(&BB);
-  if (it != BBToTargetsMap.end() && !it->second.empty()) {
-    auto    &TargetList = it->second;
-    uint32_t num_targets = TargetList.size();
-
-    // 建立一個常數陣列包含 [ID0, Dist0, ID1, Dist1, ...]
-    std::vector<Constant *> ArrayElems;
-    for (auto &p : TargetList) {
-      ArrayElems.push_back(ConstantInt::get(Int32Ty, p.first));   // target ID
-      ArrayElems.push_back(ConstantInt::get(Int32Ty, p.second));  // Distance
-    }
-
-    ArrayType      *ArrTy = ArrayType::get(Int32Ty, num_targets * 2);
-    GlobalVariable *GDistArray = new GlobalVariable(
-        *CurModule, ArrTy, true, GlobalValue::InternalLinkage,
-        ConstantArray::get(ArrTy, ArrayElems), ".afl_target_dists");
-
-    Value *ArrayPtr = IRB.CreatePointerCast(GDistArray, PtrTy);
-    FunctionCallee ReportBatchFunc = CurModule->getOrInsertFunction(
-        "__afl_report_target_batch", Type::getVoidTy(*C), Int32Ty, PtrTy);
-    IRB.CreateCall(ReportBatchFunc, 
-                   {ConstantInt::get(Int32Ty, num_targets), ArrayPtr});
-  }
 
   if (Options.TracePC) {
 
