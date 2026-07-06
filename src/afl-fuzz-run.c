14506f280ebc66a4dfc1ef72758d59b71a8eba5d
James-Lu-none <jamesabcde277@gmail.com>
2026-07-06 05:03:12 +0000

    refactor: simplify proximity score check by using average score as threshold and removing environment variable dependencies

--- src/afl-fuzz-run.c
+++ src/afl-fuzz-run.c
@@ -1154,32 +1154,12 @@ void sync_fuzzers(afl_state_t *afl) {
 
           }
 
-          u64 min_threshold = 0;
           if (afl->shm.dfg_map) {
-            min_threshold = 1;
-            double ratio = 1;
-            char *ratio_env = getenv("AFL_SYNC_PROX_RATIO");
-            if (ratio_env) {
-              ratio = atof(ratio_env);
-            }
-            if (afl->avg_prox_score > 0) {
-              u64 dynamic_threshold = (u64)(afl->avg_prox_score * ratio);
-              if (dynamic_threshold > min_threshold) {
-                min_threshold = dynamic_threshold;
-              }
-            }
-          }
-          char *min_env = getenv("AFL_SYNC_PROX_MIN");
-          if (min_env) {
-            min_threshold = strtoull(min_env, NULL, 10);
-          }
-
-          if (min_threshold > 0) {
             u64 synced_prox_score = compute_proximity_score(afl);
-            if (synced_prox_score < min_threshold) {
+            if (synced_prox_score <= afl->avg_prox_score) {
               if (unlikely(afl->debug)) {
-                DEBUGF("Skipping synced seed %s with low proximity score: %llu < %llu\n",
-                       namelist[o]->d_name, synced_prox_score, min_threshold);
+                DEBUGF("Skipping synced seed %s with current average proximity score: %llu <= %llu\n",
+                       namelist[o]->d_name, synced_prox_score, afl->avg_prox_score);
               }
               munmap(orig_mem, st.st_size);
               close(fd);
