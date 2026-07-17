61ebe712ea4c20f4ef338e8085ed7086685991a0
James-Lu-none <jamesabcde277@gmail.com>
2026-07-18 01:14:00 +0800

    fix literal newlines and mut_max_ in muoafl-v3

--- src/afl-main.c
+++ src/afl-main.c
@@ -629,25 +629,21 @@ static void save_matrices(afl_state_t *afl) {
       }
 
       // Dump mutator probability matrix to file
-      printf("Dumping mut_prob_matrix.txt...
-");
+      printf("Dumping mut_prob_matrix.txt...\n");
       u8 *mut_mat_path = alloc_printf("%s/mut_prob_matrix.txt", afl->out_dir);
       FILE *mut_f = fopen(mut_mat_path, "w");
       if (!mut_f) {
           PFATAL("Unable to create '%s'", mut_mat_path);
       }
-      fprintf(mut_f, "Mutator x Mutator Probability Matrix:
-");
+      fprintf(mut_f, "Mutator x Mutator Probability Matrix:\n");
       for (u32 ii = 0; ii < num_rows; ++ii) {
           for (u32 j = 0; j < num_cols; j++) {
               fprintf(mut_f, "%.6f ", afl->mut_probabilities[ii][j]);
           }
-          fprintf(mut_f, "
-");
+          fprintf(mut_f, "\n");
       }
       fclose(mut_f);
-      printf("Successfully dumped mut_prob_matrix.txt.
-");
+      printf("Successfully dumped mut_prob_matrix.txt.\n");
       ck_free(mut_mat_path);
 
       update_distribution(afl, afl->mut_probabilities, afl->prob_table_mut, afl->alias_table_mut, num_rows, num_cols);
@@ -679,25 +675,21 @@ static void save_matrices(afl_state_t *afl) {
       }
       
       // Dump semantic probability matrix to file
-      printf("Dumping semantic_prob_matrix.txt...
-");
+      printf("Dumping semantic_prob_matrix.txt...\n");
       u8 *sem_mat_path = alloc_printf("%s/semantic_prob_matrix.txt", afl->out_dir);
       FILE *sem_f = fopen(sem_mat_path, "w");
       if (!sem_f) {
           PFATAL("Unable to create '%s'", sem_mat_path);
       }
-      fprintf(sem_f, "Semantic Type x Mutator Probability Matrix:
-");
+      fprintf(sem_f, "Semantic Type x Mutator Probability Matrix:\n");
       for (u32 ii = 0; ii < num_semantic; ++ii) {
           for (u32 j = 0; j < num_cols; j++) {
               fprintf(sem_f, "%.6f ", semantic_probs[ii][j]);
           }
-          fprintf(sem_f, "
-");
+          fprintf(sem_f, "\n");
       }
       fclose(sem_f);
-      printf("Successfully dumped semantic_prob_matrix.txt.
-");
+      printf("Successfully dumped semantic_prob_matrix.txt.\n");
       ck_free(sem_mat_path);
 
       update_distribution(afl, semantic_probs, afl->prob_table_semantic, afl->alias_table_semantic, num_semantic, num_cols);
@@ -909,8 +901,7 @@ int main(int argc, char **argv_orig, char **envp) {
     }
 
     if (afl->in_training && get_cur_time() - afl->start_time > training_hours * 60 * 60 * 1000){
-      printf("Finished training phase, will use transition matrix P from now on...
-");
+      printf("Finished training phase, will use transition matrix P from now on...\n");
       save_matrices(afl);
       afl->in_training = false;
     }
