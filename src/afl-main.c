21eaca971517a4f60287088daa35f595497b5182
James-Lu-none <jamesabcde277@gmail.com>
2026-07-21 14:02:45 +0000

    refactor: replace mut_max_ with mut_max_global and remove afl from update_distribution

--- src/afl-main.c
+++ src/afl-main.c
@@ -31,7 +31,7 @@ extern void print_double_array(double **array, u32 size);
 extern void print_double_array_1d(double *array, u32 size);
 extern const u32 mut_max_global;
 
-void update_distribution(afl_state_t *afl, double **probabilities, double **out_prob_table, u32 **out_alias_table, u32 num_rows, u32 num_cols) {
+void update_distribution(double **probabilities, double **out_prob_table, u32 **out_alias_table, u32 num_rows, u32 num_cols) {
     for (u32 row = 0; row < num_rows; row++) {
         u32 *alias = malloc(num_cols * sizeof(u32));
         double *prob = malloc(num_cols * sizeof(double));
@@ -647,7 +647,7 @@ static void save_matrices(afl_state_t *afl) {
       printf("Successfully dumped mut_prob_matrix.txt.\n");
       ck_free(mut_mat_path);
 
-      update_distribution(afl, afl->mut_probabilities, afl->prob_table_mut, afl->alias_table_mut, num_rows, num_cols);
+      update_distribution(afl->mut_probabilities, afl->prob_table_mut, afl->alias_table_mut, num_rows, num_cols);
 
       // Compute semantic probabilities
       u32 num_semantic = 6;
@@ -693,7 +693,7 @@ static void save_matrices(afl_state_t *afl) {
       printf("Successfully dumped semantic_prob_matrix.txt.\n");
       ck_free(sem_mat_path);
 
-      update_distribution(afl, semantic_probs, afl->prob_table_semantic, afl->alias_table_semantic, num_semantic, num_cols);
+      update_distribution(semantic_probs, afl->prob_table_semantic, afl->alias_table_semantic, num_semantic, num_cols);
       for (u32 ii = 0; ii < num_semantic; ++ii) free(semantic_probs[ii]);
       free(semantic_probs);
 }
@@ -714,7 +714,7 @@ int main(int argc, char **argv_orig, char **envp) {
 
   afl_import_first(afl);  // sync peers before first cycle if AFL_IMPORT_FIRST
 
-  u32 mut_max_ = 32; // 32 mutators
+  u32 mut_max_ = mut_max_global; // 37 mutators
 
   afl->finds_per_mutator = (double **)malloc(mut_max_ * sizeof(double *));
   afl->mut_probabilities = (double **)malloc(mut_max_ * sizeof(double *));
